f416921af96f3f545d3080743ee4975ccf568eee
PacoFletcher <pacofletcher@gmail.com>
2025-03-11 10:46:03 +0100

    pulido

--- 1-RampUp/Python/Hundir la flota/una_matriz.py
+++ 1-RampUp/Python/Hundir la flota/una_matriz.py
@@ -14,30 +14,30 @@ def dentro(x,y):
         return True
     return False
 
-def mira(M,x,y):
+def mira(A,x,y):
     for i in range(-1,2):
             for j in range(-1,2):
                 if dentro(x-j,y-i):
-                    if M[x-j][y-i] != 0:
+                    if A[x-j][y-i] != 0:
                         return False
     return True
-def valido(M,l,v,j,x,y):
+def valido(A,l,v,x,y):
     # Analizaremos celda a celda.
     # Si el barco está en posición vertical, modificaremos la coordenada x 
     if v == True:
         for i in range(0,l):
-            if not mira(M,x+i + 10*j,y):
+            if not mira(A,x+i,y):
                 return False
     # Si el barco está en posición horizontal, modificaremos la coordenada y    
     else:
         for i in range(0,l):
-            if not mira(M,x + 10*j,y+i):
+            if not mira(A,x ,y+i):
                 return False
         
     return True
 
 
-def set_mapa2(M, l, B, j):
+def set_mapa2(A, l, B, j):
     # el parámetro v nos indica si el barco que estamos colocando estará en posición vertical u horizontal
     v = random.randint(0,1)
     
@@ -52,13 +52,13 @@ def set_mapa2(M, l, B, j):
             # posiciones a nuestro barco definitivamente.
             # Si valido detectara alguna casilla conflictiva, se volverían a generar 
             # dos coordenadas aleatorias hasta dar con una combinación factible.
-            if valido(M,l,v,j,x,y):
+            if valido(A,l,v,x+ 10*j,y):
                 break
         # Una vez validadas las cordenadas (x,y), procedemos a:
         # 1. Ubicar el barco en nuestro mapa
         # 2. Añadir los atributos de la clase barco a nuestra lista
         for i in range(0,l):
-            M[x+i + j*10][y] = 'B'
+            A[x+i + j*10][y] = 'B'
             t = (x+i,y)
             B[-1].coord[t] = False
         B[-1].vida = len(B[-1].coord.keys())
@@ -69,10 +69,10 @@ def set_mapa2(M, l, B, j):
         while True:
             x = random.randint(0,9)
             y = random.randint(0,9-l+1)
-            if valido(M,l,v,j,x,y):
+            if valido(A,l,v,x+ 10*j,y):
                 break
         for i in range(0,l):
-            M[x + j*10][y+i] = 'B'
+            A[x + j*10][y+i] = 'B'
             t = (x,y+i)
             B[-1].coord[t] = False
         B[-1].vida = len(B[-1].coord.keys())
@@ -80,17 +80,19 @@ def set_mapa2(M, l, B, j):
 
 A = [[0 for i in range(20)] for i in range (20)]
 B = []
-for i in (0,2):
-    for l in (4,0,-1):
-        for i in (0,5-l):
+for j in range(0,2):
+    for l in range(4,0,-1):
+        for i in range(0,5-l):
             b = barco()
             B.append(b)
             v = random.randint(0,1)
-            set_mapa2(A,l,B,i)
+            set_mapa2(A,l,B,j)
 
 
 
-funciones.imprime_tablero()
+print(A[:10][:10])
+
+funciones.imprime_tablero(A)
 
 for i in range(0,10):
     for j in range(0,10):
